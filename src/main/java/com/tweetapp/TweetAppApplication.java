0c985fc0d87efc3222380fe0ca51e8020f0aa125
AbhishekSingh612 <67855380+AbhishekSingh612@users.noreply.github.com>
2023-04-17 20:04:40 +0530

    -added swagger and actuator

--- src/main/java/com/tweetapp/TweetAppApplication.java
+++ src/main/java/com/tweetapp/TweetAppApplication.java
@@ -1,7 +1,11 @@
 package com.tweetapp;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -10,6 +14,17 @@ public class TweetAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(TweetAppApplication.class, args);
     }
+
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("Tweet App API")
+						.description("This api allows user to register, login, reset password and also create, update, delete, read tweets")
+						.version("v1.0"))
+				.externalDocs(new ExternalDocumentation()
+						.description("Github")
+						.url("https://github.com/AbhishekSingh612/tweetapp"));
+	}
 
 
 }
